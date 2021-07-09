56f78bf3914b41c1d5f6733bcc1d042f2e203e7e
Naam's Desktop <noaml12@hotmail.com>
2021-07-09 21:54:47 +0300

    added `Boop` component and hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Boop from './Components/Boop'
+
+function App()
+{
+    return (
+        <div className="App">
+            <Boop y={-7.5} >
+                <img class="App-logo" src={logo} alt="logo" />
+            </Boop>
+            <Boop y={-7.5} >
+                This is a <br/> Boop example
+            </Boop>
+            <Boop y={-7.5} >
+                <img class="App-logo" src={logo} alt="logo" />
+            </Boop>
+        </div>
+    );
 }
 
 export default App;
